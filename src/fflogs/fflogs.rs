26d1daa32bfedc5fbdb72d3d802b7ecc7d67ecc3
ade ade <ochobase@outlook.jp>
2024-01-04 22:35:59 +0900

    URLがおかしかったらループするようにした

--- src/fflogs/fflogs.rs
+++ src/fflogs/fflogs.rs
@@ -1,25 +1,32 @@
 use crate::file::file_handler::FileHandler;
-use anyhow::anyhow;
 pub struct FFlogs;
 impl FFlogs {
     pub fn url_input() -> anyhow::Result<String> {
-        let url = FileHandler::input("レポートのURLを入力してください\n例:https://ja.fflogs.com/reports/aaaaaaaaaaaaaaaaaaaaaaaaaa")?;
-        let mut url_split: Vec<&str> = url.split("/").collect();
-        if url_split.len() > 5
-            || !url_split[0].eq("https:")
-            || !url_split[2].contains("fflogs.com")
-            || !url_split[3].eq("reports")
-        {
-            return Err(anyhow!("URLを確認してください。"));
+        let mut url_split: Vec<String> = Vec::new();
+        loop {
+            let url = FileHandler::input("レポートのURLを入力してください\n例:https://ja.fflogs.com/reports/aaaaaaaaaaaaaaaaaaaaaaaaaa")?;
+            //URLが正しいかの確認
+            let a: Vec<&str> = url.split("/").collect();
+            a.iter().for_each(|e| {
+                url_split.push(e.to_string());
+            });
+            if url_split.len() > 5
+                || !url_split[0].eq("https:")
+                || !url_split[2].contains("fflogs.com")
+                || !url_split[3].eq("reports")
+            {
+                println!("URLを確認してください")
+            } else {
+                break;
+            }
         }
-        //URLに"#"が含まれていた場合
         if url_split.last().unwrap().contains("#") {
-            let param: Vec<&str> = url_split.last().unwrap().split("#").collect();
-            let reportid = param.first().unwrap();
-            url_split.remove(url_split.len() - 1);
-            url_split.push(reportid);
+           let last = url_split.last().unwrap().clone();
+           let split: Vec<&str> = last.split("#").collect();
+           url_split.pop();
+           url_split.push(split.first().unwrap().to_string())
         }
-
+        //reportidのみ取り出す
         let report_id: String = url_split
             .last()
             .unwrap()
